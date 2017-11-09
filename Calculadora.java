3e0bf6582eb02eeb3b817b9b33c47ad1420f1123
Larissa de Cassia Ferreira - 81620288 <81620288@>
2017-11-09 21:56:21 -0200

    Versao comentada

--- Calculadora.java
+++ Calculadora.java
@@ -4,7 +4,9 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
+/**
+*@author larissa
+*/
 public class Calculadora {
 	DateFormat formater = new SimpleDateFormat("dd/MM/yyyy");
 	
